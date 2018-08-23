1f047e34e4523d029a48de5d4a7d7192401d6b95
Ivan Vitali <webcoder.ivan@gmail.com>
2018-08-22 22:11:23 -0300

    se trabajo el modelo del formulario y se enlazo con el html

--- src/app/counter/order-sheet/order-sheet.component.ts
+++ src/app/counter/order-sheet/order-sheet.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { 
   FormGroup, 
   FormBuilder, 
@@ -14,16 +14,63 @@ import { CustomValidators } from '../shared/custom-validators';
   templateUrl: 'app/counter/order-sheet/order-sheet.component.html',
   styleUrls: ['app/counter/order-sheet/order-sheet.component.css']
 })
-export class OrderSheetComponent {
+export class OrderSheetComponent implements OnInit {
 
   orderSheetForm: FormGroup;
+  weirdRequestsControls: FormArray;
 
   constructor(private formBuilder: FormBuilder) {
     this.buildOrderSheetForm();
   }
+
+  ngOnInit() {
+    let customerNameControl = this.orderSheetForm.get('customerName') as FormControl;
+  }
   
   buildOrderSheetForm() {
-    this.orderSheetForm = this.formBuilder.group();
+    this.orderSheetForm = this.formBuilder.group({
+      customerName: '',
+      size: this.formBuilder.control(''),
+      bread: '',
+      specialtySandwich: '',
+      weirdRequests: this.formBuilder.array([
+        this.formBuilder.control('')
+      ]),
+      otherNotes: '',
+      meats: this.formBuilder.group({
+        meatHam: '',
+        meatTurkey: '',
+        meatRoastBeef: ''
+      }),
+      cheeses: this.formBuilder.group({
+        cheeseProvolone: '',
+        cheeseCheddar: '',
+        cheeseSwiss: ''
+      }),
+      veggiesAndSuch: this.formBuilder.group({
+        veggieLettuce: '',
+        veggieTomato: '',
+        veggieMustard: ''
+      })
+    });
+
+    this.weirdRequestsControls = this.orderSheetForm.get('weirdRequests') as FormArray;
+  }
+
+  onAddWeirdRequest() {
+    this.weirdRequestsControls.push(this.formBuilder.control(''));
+  }
+
+  onRemoveWeirdRequest(index: number) {
+    this.weirdRequestsControls.removeAt(index);
+  }
+
+  onResetForm() {
+    this.orderSheetForm.reset();
+  }
+
+  onResetWeirdRequests() {
+    this.weirdRequestsControls.reset();
   }
 
 }
